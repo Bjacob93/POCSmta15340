0fa6d26d9c1aa04efc939e91db687891dbb1b50d
dbsa11 <dbsa11@student.aau.dk>
2015-12-12 14:27:34 +0100

    mend

--- src/miniproject/OutputHanderMergeSort.java
+++ src/miniproject/OutputHanderMergeSort.java
@@ -1,5 +1,57 @@
 package miniproject;
 
-public class OutputHanderMergeSort {
+public class OutputHandlerMergeSort {
 
+	private int[] numbers;
+	private int[] helper;
+	
+	private int number;
+	
+	public void sort(int[] values){
+		
+		this.numbers = values;
+		number = values.length;
+		this.helper = new int[number];
+		mergeSort(0, number - 1);		
+	}
+	
+	private void mergeSort(int low, int high){
+	
+		if(low < high){
+			int middle = low + (high - low) / 2;
+			mergeSort(low, middle);
+			mergeSort(middle + 1, high);
+			merge(low, middle, high);
+			}		
+		
+	}	
+	
+	private void merge(int low, int middle, int high){
+		
+		for(int i = low;i <= high; i++){
+			helper[i] = numbers[i];
+		}
+		
+		int i = low;
+		int j = middle + 1;
+		int k = low;
+		
+		while(i <= middle && j <= high){
+			
+			if (helper[i] <= helper[j]){
+				numbers[k] = helper[i];
+				i++;
+			} else {
+				numbers[k] = helper[j];
+				j++;
+			}
+			k++;   
+		}
+		while(i <= middle){
+			numbers[k] = helper[i];
+			k++;
+			j++;
+		}
+	}
+	
 }
