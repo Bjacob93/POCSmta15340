ccf1bcc6b49a5b0ed3b8e46ad0d41f83bed4cef0
Bjacob93 <Zeint@live.dk>
2015-12-12 13:50:08 +0100

    inputHandler
    asd

---
+++ inputClientHandler.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class inputClientHandler extends Thread{
+	private Socket client;
+	private Scanner input;
+	private PrintWriter output;
+	
+	public inputClientHandler(Socket socket){
+		client = socket;
+		try{
+			input = new Scanner(client.getInputStream());
+			output = new PrintWriter(client.getOutputStream(), true);
+		}catch(IOException ioEx){
+			ioEx.printStackTrace();
+		}	
+	}
+	public void run(){
+		String recived;
+		String[] splitRecived;
+		do{
+		recived = input.nextLine();
+		
+		splitRecived = recived.split("\\s+");
+		
+		for (int i = 0; i < splitRecived.length; i++)
+		{
+			String word = splitRecived[i];
+			output.println(word);
+		}
+	}while(!recived.equals("quit"));
+		try{
+		if(client!=null){
+			System.out.println("Closing connection");
+			client.close();
+		}
+	}catch (IOException ioEx){
+			System.out.println("unable to close connection");
+		}
+	}
+}
