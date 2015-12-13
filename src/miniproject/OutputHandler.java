8eb6af2480d84f1477eeae4115112ee96da0702f
Seballe <sebastian@nielsen2u.dk>
2015-12-13 16:18:01 +0100

    Bubblesort
    halp

--- src/miniproject/OutputHandler.java
+++ src/miniproject/OutputHandler.java
@@ -5,39 +5,24 @@ import java.net.*;
 import java.util.*;
 
 public class OutputHandler extends Thread{
-    private static LinkedList<String> strings = multiserver.strings;
-	private static LinkedList<Integer> numbers = multiserver.numbers;
-    private static ServerSocket serverSocket;
-    private static final int PORT = 1234;
+    private static LinkedList<String> strings = MultiServer.strings;
+	private static LinkedList<Integer> numbers = MultiServer.numbers;
+	private PrintWriter output;
+	private Socket client;
     
-    public static void main(String[] args) {
-        System.out.println("Opening port ...\n");
+    private void OutputHandler(Socket socket, LinkedList<String> strings, LinkedList<Integer> numbers) {
         try {
-            serverSocket = new ServerSocket(PORT);
-        }
-        catch (IOException ioEx) {
-            System.out.println("Unable to attach to port!");
-            System.exit(1);
-        }
-        
-        do {
-            handleClient();
-        } while(true);
-    }
-
-    private static void handleClient() {
-        Socket link = null;
-        try {
-            link = serverSocket.accept();
+        	System.out.println(strings.size());
+        	client = socket;
+        	output = new PrintWriter(socket.getOutputStream(), true);
             int swap;
             String swapS;
-            PrintWriter output = new PrintWriter(link.getOutputStream(), true);
             for (int i = 0; i < numbers.size() - 1; i++){
             	for(int j = 0; j < numbers.size() - i - 1; j++){
                 	if(numbers.get(j) < numbers.get(j+1)){
                 		//swap strings
                 		swapS = strings.get(j);
-                		strings.set.(j, strings.get(j+1));
+                		strings.set(j, strings.get(j+1));
                 		strings.set(j+1, swapS);
             			//swap integers
             			swap = numbers.get(j);
@@ -47,17 +32,16 @@ public class OutputHandler extends Thread{
             	}
             }
     		for(int i = 0; i < strings.size(); i++){
-            	output.print(strings[i] + " " + numbers[i] + ", ");
+            	output.print(strings.get(i) + " " + numbers.get(i) + ", ");
             }
-            input.close();
         }
         catch (IOException ioEx) {
             ioEx.printStackTrace();
         }
-        finally {
+       // finally {
             try {
                 System.out.println("\n* Closing connection ... *");
-                link.close();
+                socket.close();
             }
             catch (IOException ioEx) {
                 System.out.println("Unable to disconnect!");
@@ -65,4 +49,4 @@ public class OutputHandler extends Thread{
             }
         }
     }
-}
+//}
