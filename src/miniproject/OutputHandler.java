8d263e71a21a1936aad4d12e138e8c5fa233129c
Seballe <sebastian@nielsen2u.dk>
2015-12-13 20:04:31 +0100

    Update OutputHandler.java

--- src/miniproject/OutputHandler.java
+++ src/miniproject/OutputHandler.java
@@ -8,45 +8,43 @@ public class OutputHandler extends Thread{
     private static LinkedList<String> strings = MultiServer.strings;
 	private static LinkedList<Integer> numbers = MultiServer.numbers;
 	private PrintWriter output;
-	private Socket client;
-    
-    private void OutputHandler(Socket socket, LinkedList<String> strings, LinkedList<Integer> numbers) {
+	
+    public OutputHandler(Socket socket) {
+    	System.out.println("starting ");
+    	String outputString = "";
         try {
-        	System.out.println(strings.size());
-        	client = socket;
         	output = new PrintWriter(socket.getOutputStream(), true);
-            int swap;
-            String swapS;
-            for (int i = 0; i < numbers.size() - 1; i++){
-            	for(int j = 0; j < numbers.size() - i - 1; j++){
-                	if(numbers.get(j) < numbers.get(j+1)){
-                		//swap strings
-                		swapS = strings.get(j);
-                		strings.set(j, strings.get(j+1));
-                		strings.set(j+1, swapS);
-            			//swap integers
-            			swap = numbers.get(j);
-            			numbers.set(j, numbers.get(j+1));
-            			numbers.set(j+1, swap);
+            	int swap;
+            	String swapS;
+        	for (int i = 0; i < numbers.size() - 1; i++){
+            		for(int j = 0; j < numbers.size() - i - 1; j++){
+                		if(numbers.get(j) < numbers.get(j+1)){
+                			//swap strings
+                			swapS = strings.get(j);
+                			strings.set(j, strings.get(j+1));
+                			strings.set(j+1, swapS);
+            				//swap integers
+            				swap = numbers.get(j);
+            				numbers.set(j, numbers.get(j+1));
+            				numbers.set(j+1, swap);
+            			}
             		}
-            	}
-            }
+        	}
     		for(int i = 0; i < strings.size(); i++){
-            	output.print(strings.get(i) + " " + numbers.get(i) + ", ");
-            }
+    			outputString += strings.get(i) + numbers.get(i);
+            	}
+    		System.out.print(outputString);
         }
         catch (IOException ioEx) {
             ioEx.printStackTrace();
         }
-       // finally {
-            try {
-                System.out.println("\n* Closing connection ... *");
+        try {
+        	System.out.println("\n* Closing connection ... *");
                 socket.close();
-            }
-            catch (IOException ioEx) {
+        }
+        catch (IOException ioEx) {
                 System.out.println("Unable to disconnect!");
                 System.exit(1);
-            }
         }
     }
-//}
+}
